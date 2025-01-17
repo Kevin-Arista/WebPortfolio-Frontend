294adafb700abddf611cb8b6d8ac0200ff5fcef8
Kevin Arista S <kevin.a8988@gmail.com>
2025-01-17 15:09:25 -0800

    created About Page

---
+++ src/pages/About/About.js
@@ -0,0 +1,13 @@
+import "./About.scss";
+import Navbar from "../components/Navbar.js";
+
+function About(props) {
+	return (
+		<div className="About">
+			<Navbar></Navbar>
+			<p>About</p>
+		</div>
+	);
+}
+
+export default About;
