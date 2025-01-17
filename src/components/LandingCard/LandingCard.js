6fdd3358be8bff1c00b7f3d8e38673628caeba0b
Kevin Arista S <kevin.a8988@gmail.com>
2025-01-17 15:28:37 -0800

    gave anchor tags links and uploaded resume

--- src/components/LandingCard/LandingCard.js
+++ src/components/LandingCard/LandingCard.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { LuLinkedin } from "react-icons/lu";
 import { TbBrandGithub } from "react-icons/tb";
 import { CiCoffeeCup } from "react-icons/ci";
+import { TbFileCv } from "react-icons/tb";
 
 function LandingCard(props) {
 	return (
@@ -13,11 +14,23 @@ function LandingCard(props) {
 					<img className="bioPicture" alt="profile_pic" src={bioPic}></img>
 					<div className="icon-row">
 						<span className="icon">
-							<LuLinkedin />
+							<a
+								href="https://www.linkedin.com/in/kevin-arista/"
+								target="_blank">
+								<LuLinkedin />
+							</a>
 						</span>
-						<span className="icon">
-							<TbBrandGithub />
-						</span>
+						<a href="https://github.com/Kevin-Arista" target="_blank">
+							<span className="icon">
+								<TbBrandGithub />
+							</span>
+						</a>
+						<a href="/docs/Resume_KevinAristaSolis.pdf" download>
+							<span className="icon">
+								<TbFileCv />
+							</span>
+						</a>
+
 						<span className="icon">
 							<CiCoffeeCup />
 						</span>
