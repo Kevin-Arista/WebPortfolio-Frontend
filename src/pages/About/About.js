c1d4dd94870d26969fc7035516b8ca9f6e460a21
Kevin Arista S <kevin.a8988@gmail.com>
2025-02-02 23:59:13 -0500

    spotify music list

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -6,6 +6,14 @@ function About(props) {
 		<div className="About">
 			<Navbar></Navbar>
 			<p>About</p>
+			<iframe
+				className="favsong"
+				src="https://open.spotify.com/embed/track/5rcnAHBclBs0OGV4rATEnR?utm_source=generator"
+				width="35%"
+				height="152"
+				allowfullscreen=""
+				allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+				loading="lazy"></iframe>
 		</div>
 	);
 }
